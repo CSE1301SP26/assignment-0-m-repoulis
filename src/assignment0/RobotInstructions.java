f2c4983a7f6492cfedcc1ec3960a438c1087dcf1
m-repoulis <m.i.repoulis@wustl.edu>
2026-01-21 11:40:08 -0600

    Assignment 0 Finished

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -31,9 +31,23 @@ public class RobotInstructions  {
 		 */
 		robot.forward(60);
 		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(100);
+		robot.turnRight(135);
+		robot.forward(90);
+		robot.turnLeft(135);
+		robot.forward(40);
+		robot.turnRight(135);
+		robot.forward(150);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(150);
+		robot.turnRight(135);
+		robot.forward(40);
+		robot.turnLeft(135);
+		robot.forward(90);
+		robot.turnRight(135);
+		robot.forward(100);
+		robot.turnLeft(90);
+		robot.forward(60);
 	}
 
 }
